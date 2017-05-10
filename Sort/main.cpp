637a7d69e7d26e92a5346bc3e0c774bdcee91dfa
trishinSV <Sergey@MacBook-Pro-Sergej.local>
2017-05-10 17:12:14 +0300

    Initial Commit

---
+++ Sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sort
+//
+//  Created by Сергей Тришин on 10.05.17.
+//  Copyright © 2017 Сергей Тришин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
